ec7590e323df6152782a21b12434ff8b68189ad8
Thang Nguyen <thang.nguyen@fruitful.io>
2015-07-24 15:48:59 +0700

    First Commit

---
+++ VinSlideView/VinSlideView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VinSlideView
+//
+//  Created by Thang Nguyen on 7/24/15.
+//  Copyright (c) 2015 Thang Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VinSlideView/VinSlideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VinSlideView
+//
+//  Created by Thang Nguyen on 7/24/15.
+//  Copyright (c) 2015 Thang Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
